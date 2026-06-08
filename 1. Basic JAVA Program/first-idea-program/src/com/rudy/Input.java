249a8aac589baeb6d6424a69eee0ac63f5953b8c
Rudraksha Bahal <rudrakshabahal@gmail.com>
2026-06-08 21:35:57 +0530

    Updated Java basics programs

--- 1. Basic JAVA Program/first-idea-program/src/com/rudy/Input.java
+++ 1. Basic JAVA Program/first-idea-program/src/com/rudy/Input.java
@@ -1,4 +1,12 @@
 package com.rudy;
 
+import java.util.Scanner;
+
 public class Input {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please Emter Your Roll no: ");
+        int rollno = input.nextInt();
+        System.out.println("Your Roll no. is " + rollno);
+    }
 }
--- 1. Basic JAVA Program/first-idea-program/src/com/rudy/Primitives.java
+++ 1. Basic JAVA Program/first-idea-program/src/com/rudy/Primitives.java
@@ -1,4 +1,16 @@
 package com.rudy;
 
 public class Primitives {
+    public static void main() {
+        int rollno  = 34;
+        char letter = 'r';
+        float marks = 98.67f;
+        double LargeDecimalNumbers = 4567654.4567;
+        long LargeInteger = 12345678901L;
+        boolean check = true;
+        
+
+
+
+    }
 }
