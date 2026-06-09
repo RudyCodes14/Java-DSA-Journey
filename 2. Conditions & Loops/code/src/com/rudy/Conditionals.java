ab7c74c271be8def314712b7c29d617fe28c0094
Rudraksha Bahal <rudrakshabahal@gmail.com>
2026-06-09 11:53:12 +0530

    updates

---
+++ 2. Conditions & Loops/code/src/com/rudy/Conditionals.java
@@ -0,0 +1,34 @@
+package com.rudy;
+
+public class Conditionals {
+    public static void main() {
+//        Syntax of if statements:
+//        if (boolean expression T or F) {
+//            // body
+//        } else {
+//            // do this
+//        }
+
+        int salary = 25400;
+//        if (salary > 10000) {
+//            salary = salary + 2000;
+//        } else {
+//            salary = salary + 1000;
+//        }
+//        System.out.println(salary);
+
+        // multiple if-else
+
+        if (salary > 10000) {
+            salary += 2000; // salary = salary + 2000
+        } else if (salary > 20000) {
+            salary += 3000;
+        } else {
+            salary += 1000;
+        }
+
+        System.out.println(salary);
+
+
+    }
+}
