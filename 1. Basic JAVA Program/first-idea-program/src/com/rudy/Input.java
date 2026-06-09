9fad470cb2c5730505c3c1f441ff37587e88a484
Rudraksha Bahal <rudrakshabahal@gmail.com>
2026-06-09 10:29:10 +0530

    updates

--- 1. Basic JAVA Program/first-idea-program/src/com/rudy/Input.java
+++ 1. Basic JAVA Program/first-idea-program/src/com/rudy/Input.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Please Emter Your Roll no: ");
+        System.out.print("Please Enter Your Roll no: ");
         int rollno = input.nextInt();
         System.out.println("Your Roll no. is " + rollno);
     }
---
+++ 1. Basic JAVA Program/first-idea-program/src/com/rudy/Temperature.java
@@ -0,0 +1,15 @@
+package com.rudy;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please Enter Temp in C: ");
+        float tempC = in.nextFloat();
+        float tempF = (tempC * 9/5) + 32;
+        System.out.println(tempF);
+    }
+}
